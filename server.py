b5c3dade9c88aada7e84526bea40ae9cbb6c3418
Schuyler Alschuler <alschuler.s@gmail.com>
2020-04-07 22:53:37 +0000

    Adds basic server code

--- server.py
+++ server.py
@@ -1,9 +1,22 @@
-#! python3
-# Server for univeral clipboard
 
-from flask import Flask
+# A very simple Flask Hello World app for you to get started with...
+
+from flask import Flask, request
+
 app = Flask(__name__)
 
 @app.route('/')
-def root():
-    return 'Hello, world!'
+def hello_world():
+    return 'Hello from Flask!'
+
+@app.route('/clipboard', methods=['GET', 'POST'])
+def accessClipboard():
+    contents = "Nothing to paste"
+    if request.method == 'POST':
+        with open('clipboard.txt', 'w') as f:
+            f.write(request.form['value'] + '\n')
+    if request.method == 'GET':
+        with open('clipboard.txt', 'r') as f:
+            contents = f.read()
+            return contents
+    return "Bad method."
